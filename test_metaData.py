a2b6c62d2b9ce8e114b23b53d78538bcbba1970d
bussiere <bussiere@gmail.com>
2017-02-27 16:19:46 +0100

    test

--- test_metaData.py
+++ test_metaData.py
@@ -8,16 +8,24 @@ import bs4 as BeautifulSoup
 
 
 # In[8]:
-
-title, description, image = web_preview("aurl.com")
+url = "http://www.streetpress.com/sujet/1488190869-tribunal-de-l-armee"
+title, description, image = web_preview(url)
 
 
 # In[7]:
-
-print(title)
-
-
-# In[ ]:
-
+template = """<html><head><meta name="twitter:card" content="" />
+<meta name="twitter:site" content="" />
+<meta name="twitter:title" content="%s" />
+<meta name="twitter:description" content="%s" />
+<meta name="twitter:image" content="%s" />
+<meta property="og:type" content="website">
+<meta property="og:title" content="%s">
+<meta property="og:description" content="%s">
+<meta property="og:url" content="%s">
+<meta property="og:image" content="%s">
+</head><body></body></html>"""%(title,description,image,title,description,url,image)
+f = open('index.html', 'w')
+f.write(template)
+f.close()
 
 
