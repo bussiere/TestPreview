73f66c7ff553f2899ef01de9ad103526369dbf6a
bussiere <bussiere@gmail.com>
2017-02-27 16:05:45 +0100

    rajout

---
+++ test_metaData.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# In[6]:
+
+from webpreview import web_preview
+import bs4 as BeautifulSoup
+
+
+# In[8]:
+
+title, description, image = web_preview("aurl.com")
+
+
+# In[7]:
+
+print(title)
+
+
+# In[ ]:
+
+
+
